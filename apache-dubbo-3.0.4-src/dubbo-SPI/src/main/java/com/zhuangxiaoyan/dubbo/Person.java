abf64509133bbf0d445cbab2191804393d65a98c
xjl <18279148786@163.com>
2021-12-08 23:54:44 +0800

    feat(Dubbo) [xjl011] add dubbo SPI demo

---
+++ apache-dubbo-3.0.4-src/dubbo-SPI/src/main/java/com/zhuangxiaoyan/dubbo/Person.java
@@ -0,0 +1,10 @@
+package com.zhuangxiaoyan.dubbo;
+
+/**
+ * @Classname Person
+ * @Description TODO
+ * @Date 2021/12/8 23:00
+ * @Created by xjl
+ */
+public interface Person {
+}
---
+++ apache-dubbo-3.0.4-src/dubbo-SPI/src/main/java/com/zhuangxiaoyan/dubbo/PersonImpl.java
@@ -0,0 +1,10 @@
+package com.zhuangxiaoyan.dubbo;
+
+/**
+ * @Classname PersonImpl
+ * @Description TODO
+ * @Date 2021/12/8 23:01
+ * @Created by xjl
+ */
+public class PersonImpl implements Person{
+}
